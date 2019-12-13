3bafa6d2641380bf558696114689e06f74e356c1
Neiko2642 <Efstathiosprantalos@outlook.com>
2019-12-13 08:59:02 -0600

    Update starterKantoPokemon.js
    added in starter pokemon object

--- v1.5 (check)/pokemonjson/starterKantoPokemon.js
+++ v1.5 (check)/pokemonjson/starterKantoPokemon.js
@@ -1,1 +1,35 @@
+let starterKantoPokemon = [{
+    "name": "Charmander",
+    "type": "fire",
+    "hp": 70,
+    "speed": "40",
+    "weaknesses": ["ground", "rock", "water"],
+    "attack" : {
+        "name" : "Fire Fang",
+        "hp" : "20"
+    }
+},
+{
+    "name": "Bulbasaur",
+    "type": ["Grass", "Poison"],
+    "hp": "70",
+    "speed": "30",
+    "weaknesses": ["Fire", "Flying", "Ice", "Psychic"],
+    "attack" : {
+        "name" : "Vine Whip",
+        "hp" : "50"
 
+    }
+},
+{
+    "name": "Squirtle",
+    "type": "Water",
+    "hp": "60",
+    "speed": "20",
+    "weaknesses": ["Electric", "Grass"],
+    "attack" : {
+        "name" : "Rain Splash",
+        "hp" : "60"
+
+    }
+}];
