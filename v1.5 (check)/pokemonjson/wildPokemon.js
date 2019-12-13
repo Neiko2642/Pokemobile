9cde7e21a8a3916257c5f12a7e45d4598c3ae7f3
Neiko2642 <Efstathiosprantalos@outlook.com>
2019-12-13 09:00:05 -0600

    Update wildPokemon.js
    added wildpokes

--- v1.5 (check)/pokemonjson/wildPokemon.js
+++ v1.5 (check)/pokemonjson/wildPokemon.js
@@ -1,1 +1,14 @@
+let wildPokemon = [{
+        "name": "Diglett",
+        "type": "Ground",
+        "hp": "50",
+        "speed": "50",
+        "weaknesses": ["Grass", "Ice", "Water"],
+        "attack" : {
+            "name" : "Hook",
+            "hp" : "30"
+    
+        }
+    },
 
+];
