c3dede260e4f573a80fa3e594aa1565d74bb4c94
Neiko2642 <Efstathiosprantalos@outlook.com>
2019-12-20 04:02:58 -0500

    Update pokeStore.js
    added poke store

--- v1.5 (check)/pokemonjson/pokeStore.js
+++ v1.5 (check)/pokemonjson/pokeStore.js
@@ -1,1 +1,21 @@
 
+let pokeStore = [{
+    "item" : "Poke Ball",
+    "hp": "0",
+    "price" : "200"
+},
+{
+    "item" : "Potion",
+    "hp": 100,
+    "price" : "300"
+},
+{
+    "item" : "Antidote",
+    "hp": "0",
+    "price" : "100"
+},
+{
+    "item" : "Parlyz Heal",
+    "hp": "0",
+    "price" : "200"
+}];
