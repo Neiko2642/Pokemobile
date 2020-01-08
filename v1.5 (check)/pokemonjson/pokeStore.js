a64bee3c5449979e0cf103efda8a86e3a0c0ec55
JonnyJJ24 <49874463+JonnyJJ24@users.noreply.github.com>
2020-01-08 08:22:58 -0600

    Add files via upload

--- v1.5 (check)/pokemonjson/pokeStore.js
+++ v1.5 (check)/pokemonjson/pokeStore.js
@@ -1,21 +1,24 @@
-
 let pokeStore = [{
     "item" : "Poke Ball",
     "hp": "0",
-    "price" : "200"
+    "price" : "200",
+    "amount": 0
 },
 {
     "item" : "Potion",
     "hp": 100,
-    "price" : "300"
+    "price" : "300",
+    "amount": 0
 },
 {
     "item" : "Antidote",
     "hp": "0",
-    "price" : "100"
+    "price" : "100",
+    "amount": 0
 },
 {
     "item" : "Parlyz Heal",
     "hp": "0",
-    "price" : "200"
+    "price" : "200",
+    "amount": 0
 }];
